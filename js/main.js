0ac119e18a28c1e5eeebbe18f6b2cee5aa8bca77
Admin <admin@MacBook-Pro-Admin.local>
2019-08-16 15:17:26 +0300

    first-commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$('.single-item').slick({
+	infinite: true,
+	slidesToShow: 1,
+	slodesToScroll: 1,
+	arrows: false,
+	dots: true
+});
+$('.logo-item').slick({
+	infinite: true,
+	slidesToShow: 1,
+	slodesToScroll: 1,
+	arrows: false,
+	dots: false
+});
